d6cacc348695f69eac0d703ffd70374056200cf6
MatthieuCarlier <144430700+MatthieuCarlier@users.noreply.github.com>
2023-12-19 10:58:05 +0100

    Add files via upload

---
+++ vscarte/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        fond : 'rgb(var(--color-fond) / 0.5)',
+      }
+    },
+  },
+  plugins: [
+  ],
+}
